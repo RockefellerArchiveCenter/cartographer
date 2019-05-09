9c3a9a95bde907f21694e56a3c7e270ccde4d001
Hillel Arnold <helrond@hotmail.com>
2019-05-09 16:26:55 -0400

    remove unnecessary js

--- maps/static/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click', '.collapse-icon', function(){
-  var target = $(this).attr('href')
-  $.ajax({
-      url: $(target).data('src')})
-    .done(function(data) {
-      $(target).append(data)
-    });
-});
