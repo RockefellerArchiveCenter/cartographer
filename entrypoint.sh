292d0ee4d1dc6b0c5c09b78786e789b5cc2af157
Hillel Arnold <helrond@hotmail.com>
2019-06-19 10:37:09 -0400

    updating configs for better runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./wait-for-it.sh cartographer-db:5432 -- echo "Creating config file"
-
 if [ ! -f manage.py ]; then
   cd cartographer
 fi
+
+./wait-for-it.sh cartographer-db:5432 -- echo "Creating config file"
 
 if [ ! -f cartographer/config.py ]; then
     cp cartographer/config.py.example cartographer/config.py
@@ -19,4 +19,4 @@ python manage.py makemigrations && python manage.py migrate
 
 #Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8007
